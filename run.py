27c57b0b3b76729055016c3fb721b2bde596ed8a
Cameron Stewart <cameron.stewart246@gmail.com>
2019-02-22 22:44:07 +0000

    Removes chain code

--- run.py
+++ run.py
@@ -1,36 +1,15 @@
-from markov_chain_lyrics import *
+from get_lyrics import *
 
 
 def ask_user():
 	# Function asks user for lyrics or Markov Chains
 
-	markov_chain_choices = ['markov chain', 'markov', 'mc', 'chain', 'markov chains', 'markovchain', 'markovchains', 'markov_chain', 'markov_chains']
-	lyrics_choices = ['lyrics', 'lyric', 'l']
-	# List of accepted inputs
-	
-	ask_again = True
-	# Initialises while loop
+	normal_lyrics()
+	#Calls normal_lyrics function from get_lyrics
 
-	while ask_again == True:
-		# While loop asks for input until accepted input is eneterd
 
-		user_choice = input('Choose Markov Chains or Lyrics: ')
-		user_choice = user_choice.lower()
-		# Formats user input
 
-		if user_choice in markov_chain_choices:
-			markov_chain()
-			ask_again = False
 
-		elif user_choice in lyrics_choices:
-			normal_lyrics()
-			ask_again = False
-
-		else:
-			print('Invalid choice, try again. ')
-		# Pick funtion user chooses
-
-	
 	another_choice = True
 		# Initialises loop for second choice
 
