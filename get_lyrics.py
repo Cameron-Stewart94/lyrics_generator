857fb45692fe47c7409adcb6ed6065b57e0beb1f
Cameron Stewart <cameron.stewart246@gmail.com>
2019-02-23 00:04:44 +0000

    Writes .txt file to desktop

--- get_lyrics.py
+++ get_lyrics.py
@@ -1,5 +1,6 @@
 from fetch_data import data
 from gtts import gTTS
+import os
 
 def normal_lyrics():
 	# Function to create file with song lyrics and play them out loud
@@ -7,7 +8,9 @@ def normal_lyrics():
 	song = data()
 	# Gets lyrics from genius.com using data function in fetch_data
 
-	with open('song_lyrics.txt', 'w', encoding= 'utf-8') as text_file:
+	file_location = os.path.join(os.environ['USERPROFILE'], 'Desktop', 'songs', 'lyrics.txt')
+
+	with open(file_location, 'w', encoding= 'utf-8') as text_file:
 		text_file.write(song)
 		# Writes lyrics to a text file
 
