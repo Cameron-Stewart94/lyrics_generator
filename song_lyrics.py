d75638bf8926a9ee6c293f5418b5aa2045b7716b
Cameron Stewart <cameron.stewart246@gmail.com>
2019-02-27 17:27:10 +0000

    Variable name updates

--- song_lyrics.py
+++ song_lyrics.py
@@ -8,46 +8,47 @@ from titlecase import titlecase
 class lyrics:
 # Class to generate song lyrics from user input
 
-    def __init__(self, artist, song, song_lyrics=''):
+    def __init__(self, artist_name, song_name, song_lyrics=''):
         # Function initialises lyrics class
-        self.artist = artist
-        self.song = song
+        self.artist_name = artist_name
+        self.song_name = song_name
         self.song_lyrics = song_lyrics
 
     def __repr__(self):
         # Function return song name and artis name when printed
-        choice_message = 'You chose {song}, by {artist}'.format(song=self.song, artist=self.artist)
-        return titlecase(choice_message)
+        user_message = 'You chose {song}, by {artist}'.format(song=self.song_name, artist=self.artist_name)
+        return titlecase(user_message)
 
     def fetch_data(self):
         #Function fetches lyrics from genius
-        artist = self.artist
-        artist = artist.lower()
-        artist = artist[0].upper() + artist[1:]
-        artist = artist.split()
-        artist = '-'.join(artist)
+        artist_name = self.artist_name
+        artist_name = artist_name.lower()
+        artist_name = artist_name[0].upper() + artist_name[1:]
+        artist_name = artist_name.split()
+        artist_name = '-'.join(artist_name)
         # Takes artist input and formats string as needed in genius url
 
-        song = self.song
-        song = song.split()
-        song = '-'.join(song)
+        song_name = self.song_name
+        song_name = song_name.split()
+        song_name = '-'.join(song_name)
         # Takes song input and formats as needed in genius url
 
-        title = artist + '-' + song + '-lyrics'
+        title = artist_name + '-' + song_name + '-lyrics'
         website = 'http://genius.com/{title}'.format(title = title)
         # Creates full web address using formated user inputs
 
         source = requests.get(website).text
         soup = BeautifulSoup(source, 'lxml')
-        div = soup.find('div', class_ = 'lyrics').text
+        html_lyrics = soup.find('div', class_ = 'lyrics').text
         # Scraps desired song from genius.com using requests and BeautifulSoup
 
-        self.song_lyrics += div
-        return div
+        self.song_lyrics += html_lyrics
+        return html_lyrics
 
     def write_to_txt_file(self):
         # Funtion writes song lyrics to txt file and saves on PC
-        txt_file_location = os.path.join(os.environ['USERPROFILE'], 'Python', 'Song Lyrics', 'lyrics.txt')
+
+        txt_file_location = os.path.join(os.environ['USERPROFILE'], 'Python', 'Song Lyrics', 'song_lyrics.txt')
         # Creates path to save .txt file(c://Ellie/Desktop/songs)
 
         with open(txt_file_location, 'w', encoding= 'utf-8') as text_file:
@@ -58,15 +59,15 @@ class lyrics:
 
     def write_to_mp3_file(self):
         # Function writes song lyrics to mp3 file and saves on PC
-        mp3_file_location = os.path.join(os.environ['USERPROFILE'], 'Python', 'Song Lyrics', 'lyrics.mp3')
+        mp3_file_location = os.path.join(os.environ['USERPROFILE'], 'Python', 'Song Lyrics', 'song_lyrics.mp3')
         # Creates path to save .txt file(c://Ellie/Desktop/songs)
 
         remove_verse_headings = self.song_lyrics
         remove_verse_headings = re.sub("\[[^]]+\]", "", remove_verse_headings)
         # Uses re to remove words in [] brackets - this removes verse heading so tts doesn't read them
 
-        sing = gTTS(remove_verse_headings)
-        sing.save(mp3_file_location)
+        tts = gTTS(remove_verse_headings)
+        tts.save(mp3_file_location)
         # Creates mp3 file of song
 
         return
