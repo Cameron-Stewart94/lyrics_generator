0ebc0a881bf2c66c5697a65b5b8245f335eee286
Cameron Stewart <cameron.stewart246@gmail.com>
2019-03-02 11:21:08 +0000

    Test

--- run.py
+++ run.py
@@ -8,25 +8,20 @@ def run_programme():
     song_lyrics_generator = lyrics(user_choices['Artist'], user_choices['Song'])
     # Creates instance of song_lyrics_generator
 
-    try:
-        song_lyrics_generator.fetch_data()
-    except:
-        print('Invalid choice, try again...')
-        run_programme()
-    else:
-        print(song_lyrics_generator)
-        #Lets user know their choice of song
 
-        song_lyrics_generator.write_to_txt_file()
+    song_lyrics_generator.fetch_data()
+
+
+    song_lyrics_generator.write_to_txt_file()
         # Writes song to text file
 
-        song_lyrics_generator.write_to_mp3_file()
+    song_lyrics_generator.write_to_mp3_file()
         # Writes song to mp3 file
 
-        another_song = ask_user_again()
+    another_song = ask_user_again()
         # Asks user if they would like to choose another song
 
-        if another_song == True:
+    if another_song == True:
             run_programme()
             # Runs Programme again if user asks it to
 
