1f90cdd1c93ef7e493db07f38a6af4e0e9615823
Cameron Stewart <cameron.stewart246@gmail.com>
2019-03-07 15:58:27 +0000

    Run again

--- run.py
+++ run.py
@@ -11,12 +11,15 @@ def run_programme():
     'Save as mp3 file' : ['3', 'mp3', 'mp3 file', 'save as mp3 file'],
     'Quit' : ['4', 'q', 'quit', 'exit']
     }
+    # Creates dictionary for user input to choose an option from
 
     with open('menus/main_menu.txt', 'r') as menu_file:
         menu = [line for line in menu_file]
+    # Opens main menu text file and stores each line in a list
 
     user_choices = ask_user()
     song_lyrics_generator = lyrics(user_choices['Artist'], user_choices['Song'])
+    # Asks user for artisit and song choice and creates instance of song lyrics generator class using the user's inputs
 
     try:
         song_lyrics_generator.fetch_data()
@@ -24,6 +27,7 @@ def run_programme():
         try_again = input('Invalid choice, try again? ').lower()
         if try_again == 'y' or try_again == 'Y':
             run_programme()
+        # Attemps to fetch song data from the web, if invalid choice is entered, the programme asks the user to try again
     else:
         quit = False
         while quit == False:
@@ -40,6 +44,9 @@ def run_programme():
 
             elif menu_choice in method_options['Save as mp3 file']:
                 song_lyrics_generator.write_to_mp3_file()
+
+            elif menu_choice in method_options['Choose another song']:
+                run_programme()
 
             elif menu_choice in method_options['Quit']:
                 quit = True
